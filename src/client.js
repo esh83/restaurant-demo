45af98fec79bdc0fe9f57b029dc076e0b3ef8f5f
Ehsan SH <56648542+esh83@users.noreply.github.com>
2021-07-13 23:07:05 +0430

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,6 +1,5 @@
 import {createClient} from 'contentful'
-import env from "react-dotenv";
 export const client = createClient({
-    space : env.REACT_APP_SPACE,
-    accessToken : env.REACT_APP_ACCESS_TOKEN
+    space : process.env.REACT_APP_SPACE,
+    accessToken : process.env.REACT_APP_ACCESS_TOKEN
 })
